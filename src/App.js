7543d71518cea48458c900b0df53456a0ffae314
Frank Riccobono <F.Riccobono@gmail.com>
2022-01-13 23:18:54 -0500

    add win/loss, better styling, and word list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 import styles from "./App.module.css";
+import wordList from './wordlist'
 
 const characters = [
   "a",
@@ -40,24 +41,40 @@ const characters = [
   "θ",
 ];
 
-const solution = "wɔrɡə";
+const start = new Date("Jan 12, 2022")
+const days = Math.floor((new Date() - start) / (1000*60*60*24))
+
+const solution = wordList[days][1];
+
+const wordMap = {}
+wordList.forEach (word => wordMap[word[1]] = 1)
+
 
 function reduce(state, action) {
   const guesses = [...state.guesses];
+  const currentGuess = guesses[state.currentGuess];
   switch (action.type) {
     case "GUESS_LETTER":
-      guesses[state.currentGuess] += action.payload;
-      return { ...state, guesses: guesses };
+      if (currentGuess.length < 5 && !state.win && !state.loss) {
+        guesses[state.currentGuess] += action.payload;
+        return { ...state, guesses: guesses };
+      } else {
+        return state;
+      }
     case "BACKSPACE":
-      guesses[state.currentGuess] = guesses[state.currentGuess].substring(
+      guesses[state.currentGuess] = currentGuess.substring(
         0,
-        guesses[state.currentGuess].length - 1
+        currentGuess.length - 1
       );
       return { ...state, guesses: guesses };
     case "CHECK_WORD":
-      if (guesses[state.currentGuess].length === 5) {
+      if (currentGuess.length === 5) {
+        if(wordMap[currentGuess] !== 1){
+          alert("not a word")
+          return state;
+        }
         const newLetterMap = new Map(state.letterMap);
-        Array.from(guesses[state.currentGuess]).forEach((letter, pos) => {
+        Array.from(currentGuess).forEach((letter, pos) => {
           if (newLetterMap.get(letter) !== styles.correct) {
             newLetterMap.set(letter, checkLetter(letter, pos, solution));
           }
@@ -66,6 +83,8 @@ function reduce(state, action) {
           ...state,
           currentGuess: state.currentGuess + 1,
           letterMap: newLetterMap,
+          win: currentGuess === solution,
+          loss: state.currentGuess + 1 === 6,
         };
       } else {
         return state;
@@ -86,16 +105,32 @@ function checkLetter(letter, position, word) {
   return state;
 }
 
-function Letter({ letter, position, clean }) {
-  let state = styles.incorrect;
+function Modal({children}){
+  const [visible, setVisible] = useState("")
 
-  if (clean) {
-    state = styles.clean;
-  } else if (solution.charAt(position) === letter) {
-    state = styles.correct;
-  } else if (solution.includes(letter)) {
-    state = styles.partial;
+  requestAnimationFrame(()=>{
+    setVisible(styles.visible)
+  })
+  return <div className={`${styles.modal} ${visible}`}>
+    {children}
+  </div>
+}
+
+function Letter({ letter, position, clean }) {
+  const [state, setState] = useState(styles.clean);
+
+  if (!clean) {
+    let newState = styles.incorrect;
+    if (solution.charAt(position) === letter) {
+      newState = styles.correct;
+    } else if (solution.includes(letter)) {
+      newState = styles.partial;
+    }
+    requestAnimationFrame(() => {
+      setState(newState);
+    });
   }
+
   return <li className={`${styles.guess_letter} ${state}`}>{letter}</li>;
 }
 
@@ -120,33 +155,42 @@ function App() {
 
   return (
     <div className="App">
-      {state.guesses.map((guess, i) => (
-        <ul key={i} className={styles.guess_word}>
-          {Array.from(guess.padEnd(5, " ")).map((letter, pos) => (
-            <Letter
-              key={pos}
-              letter={letter}
-              position={pos}
-              clean={i >= state.currentGuess}
-            />
-          ))}
-        </ul>
-      ))}
-      {characters.map((c) => (
-        <button
-          className={`${styles.ipa_button} ${state.letterMap.get(c)}`}
-          key={c}
-          onClick={() => onclick(c)}
-        >
-          {c}
+      {state.win && <Modal><p>👏 You win!</p></Modal>}
+      {state.loss && <Modal><p>Better luck next time</p></Modal>}
+      <header>
+        <h1>/wɚdl̩/</h1>
+      </header>
+      <main>
+        {state.guesses.map((guess, i) => (
+          <ul key={i} className={styles.guess_word}>
+            {Array.from(guess.padEnd(5, " ")).map((letter, pos) => (
+              <Letter
+                key={pos}
+                letter={letter}
+                position={pos}
+                clean={i >= state.currentGuess}
+              />
+            ))}
+          </ul>
+        ))}
+      </main>
+      <div className={styles.keyboard}>
+        {characters.map((c) => (
+          <button
+            className={`${styles.ipa_button} ${state.letterMap.get(c)}`}
+            key={c}
+            onClick={() => onclick(c)}
+          >
+            {c}
+          </button>
+        ))}
+        <button className={styles.ipa_button} onClick={backspace} style={{gridColumn: "6 /span 2"}}>
+          ⌫
         </button>
-      ))}
-      <button className={styles.ipa_button} onClick={backspace}>
-        ⌫
-      </button>
-      <p>
-        <button onClick={check}>Submit</button>
-      </p>
+        <button className={styles.ipa_button} onClick={check} style={{gridColumn: "8 /span 3"}}>
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
